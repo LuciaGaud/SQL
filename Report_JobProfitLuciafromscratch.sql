ad71ea012d958072dbdce7e043a208ff1aacb771
MarleneGray <marlenegray666@gmail.com>
2025-01-27 15:21:04 +0000

    added the filtering by department feature

--- Report_JobProfitLuciafromscratch.sql
+++ Report_JobProfitLuciafromscratch.sql
@@ -14,17 +14,28 @@ RETURN
     SELECT 
         COALESCE(A.JH_LocalClientCode, B.JH_LocalClientCode) AS JH_LocalClientCode,
         COALESCE(A.JH_LocalClientName, B.JH_LocalClientName) AS JH_LocalClientName,
-        ISNULL(A.WIPAmount, 0)  AS A_WIPAmount,
-        ISNULL(B.WIPAmount, 0)  AS B_WIPAmount,
-        ISNULL(A.CSTAmount, 0)  AS A_CSTAmount,
-        ISNULL(B.CSTAmount, 0)  AS B_CSTAmount,
-        ISNULL(A.ACRAmount, 0)  AS A_ACRAmount,
-        ISNULL(B.ACRAmount, 0)  AS B_ACRAmount,
-        ISNULL(A.REVAmount, 0)  AS A_REVAmount,
-        ISNULL(B.REVAmount, 0)  AS B_REVAmount,
-        ISNULL(A.JH_Profit, 0)  AS A_JH_Profit,
-        ISNULL(B.JH_Profit, 0)  AS B_JH_Profit,
-        @JH_DepartmentPKList AS DepartmentList -- New field added
+        ISNULL(A.WIPAmount, 0) AS A_WIPAmount,
+        ISNULL(B.WIPAmount, 0) AS B_WIPAmount,
+        ISNULL(A.CSTAmount, 0) AS A_CSTAmount,
+        ISNULL(B.CSTAmount, 0) AS B_CSTAmount,
+        ISNULL(A.ACRAmount, 0) AS A_ACRAmount,
+        ISNULL(B.ACRAmount, 0) AS B_ACRAmount,
+        ISNULL(A.REVAmount, 0) AS A_REVAmount,
+        ISNULL(B.REVAmount, 0) AS B_REVAmount,
+        ISNULL(A.JH_Profit, 0) AS A_JH_Profit,
+        ISNULL(B.JH_Profit, 0) AS B_JH_Profit,
+        A.JobCount AS A_JobCount, -- Number of unique JE_PK in A
+        B.JobCount AS B_JobCount, -- Number of unique JE_PK in B
+        @JH_DepartmentPKList AS DepartmentList, -- New field added
+        -- New Column: ActualPercentage
+        CASE 
+            WHEN ISNULL(A.JH_Profit, 0) != 0 THEN 
+                CAST(
+                    (ISNULL(B.JH_Profit, 0) - ISNULL(A.JH_Profit, 0)) AS FLOAT
+                ) / ABS(CAST(ISNULL(A.JH_Profit, 0) AS FLOAT))
+            ELSE 
+                NULL 
+        END AS ActualPercentage
     FROM
     (
         -- First date range data
@@ -35,7 +46,8 @@ RETURN
             SUM(CSTAmount) AS CSTAmount,
             SUM(ACRAmount) AS ACRAmount,
             SUM(REVAmount) AS REVAmount,
-            SUM(AL_LineAmount) AS JH_Profit
+            SUM(AL_LineAmount) AS JH_Profit,
+            COUNT(DISTINCT JE_PK) AS JobCount
         FROM  
             JobDeclaration
         INNER JOIN 
@@ -45,13 +57,22 @@ RETURN
         LEFT JOIN 
             OrgHeader AS LocalClient ON LocalClientAddress.OA_OH = LocalClient.OH_PK 
         OUTER APPLY 
-            dbo.csfn_TransactionLinesForPeriod(@JH_FromRevenueRecognizedDate, DATEADD(DAY, 1, @JH_ToRevenueRecognizedDate), 'N', 'N', JH_PK) --because the fime is 00:00 so it does not inlcude the last day
-                     
+            dbo.csfn_TransactionLinesForPeriod(@JH_FromRevenueRecognizedDate, DATEADD(DAY, 1, @JH_ToRevenueRecognizedDate), 'N', 'N', JH_PK) TLF
+        WHERE 
+            (
+                -- Filter for AL_GE in the list if @JH_DepartmentPKList is not empty
+                (TLF.AL_GE IN (
+                    SELECT TRY_CAST(value AS UNIQUEIDENTIFIER)
+                    FROM STRING_SPLIT(REPLACE(@JH_DepartmentPKList, '''', ''), ',')
+                    WHERE TRY_CAST(value AS UNIQUEIDENTIFIER) IS NOT NULL
+                ))
+                OR 
+                -- Show all transactions if @JH_DepartmentPKList is NULL or empty
+                @JH_DepartmentPKList IS NULL
+            )             
         GROUP BY 
             LocalClient.OH_Code,
             LocalClient.OH_FullName
-       /* HAVING 
-        SUM(AL_LineAmount) > 0 */
     ) AS A
     FULL OUTER JOIN
     (
@@ -64,7 +85,12 @@ RETURN
             SUM(ACRAmount) AS ACRAmount,
             SUM(REVAmount) AS REVAmount,
             SUM(AL_LineAmount) AS JH_Profit,
-            @JH_DepartmentPKList AS DepartmentList -- New field added
+            (SELECT COUNT(DISTINCT JE_PK)
+         FROM JobDeclaration JD
+         INNER JOIN JobHeader JH ON JD.JE_PK = JH.JH_PK
+         WHERE JD.LocalClient_OH = LocalClient.OH_PK
+         AND JD.SomeFilterConditions = B.SomeCondition
+        ) AS JobCount
         FROM  
             JobDeclaration
         INNER JOIN 
@@ -74,13 +100,12 @@ RETURN
         LEFT JOIN 
             OrgHeader AS LocalClient ON LocalClientAddress.OA_OH = LocalClient.OH_PK 
         OUTER APPLY 
-            dbo.csfn_TransactionLinesForPeriod(@JH_FromRevenueRecognizedDate2, DATEADD(DAY, 1, @JH_ToRevenueRecognizedDate2), 'N', 'N', JH_PK) --because the fime is 00:00 so it does not inlcude the last day
-
+            dbo.csfn_TransactionLinesForPeriod(@JH_FromRevenueRecognizedDate2, DATEADD(DAY, 1, @JH_ToRevenueRecognizedDate2), 'N', 'N', JH_PK)
         GROUP BY 
             LocalClient.OH_Code,
             LocalClient.OH_FullName
     ) AS B
     ON A.JH_LocalClientCode = B.JH_LocalClientCode
     WHERE
-    A.REVAmount >0 
+        A.REVAmount > 0 OR B.REVAmount > 0
 );
